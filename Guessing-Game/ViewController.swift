a8aec7a486e34635570998d161acc09c82c4405b
Kyle Houts <kyle.elizabeth.houts@gmail.com>
2018-09-24 13:21:15 -0500

    added UI objects

--- Guessing-Game/ViewController.swift
+++ Guessing-Game/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var gameTitle: UILabel!
+    
+    @IBOutlet weak var guessNumberTextbox: UITextField!
+    
+    @IBOutlet weak var guessStatusLabel: UILabel!
+    
+    @IBOutlet weak var submitButton: UIButton!
+    
+    @IBOutlet weak var remainingAttemptsLabel: UILabel!
+    
+    @IBOutlet weak var remainingAttemptsNumber: UILabel!
+    
+    @IBOutlet weak var resetGameButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    
 }
 

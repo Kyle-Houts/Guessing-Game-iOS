d133567d9019726e7bcfeaa29b82b357a7e14ad0
Kyle Houts <kyle.elizabeth.houts@gmail.com>
2018-09-25 13:25:33 -0500

    Saving point

--- Guessing-Game/ViewController.swift
+++ Guessing-Game/ViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var gameTitle: UILabel!
+    @IBOutlet weak var guessingGameTitle: UILabel!
     
-    @IBOutlet weak var guessNumberTextbox: UITextField!
+    @IBOutlet weak var guessTextbox: UITextField!
     
     @IBOutlet weak var guessStatusLabel: UILabel!
     
@@ -24,13 +24,71 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var resetGameButton: UIButton!
     
+    @IBOutlet weak var spinner: UIPickerView!
     
+    var getPlayAgainInput: String?
+    
+    var correctNumber = Int(arc4random_uniform(100)) + 1
+    
+    var numberOfTries = 8
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    @IBAction func clickSubmitButton(_ sender: Any) {
+        
+        
+        // TODO:- make sure it won't accept a string
+        
+        guessStatusLabel.text = "Good luck!"
+        
+        var guessInt: Int? = nil
+        
+        while guessInt == nil {
+            let priorityInput = guessTextbox.text
+            guessInt = Int(priorityInput!)
+            if let unwrappedGuess = guessInt {
+                if unwrappedGuess < 1 || unwrappedGuess > 100 {
+                    guessStatusLabel.text = "Please enter a valid number"
+                    guessInt = nil
+                }
+            } else {
+                guessStatusLabel.text = "Invalid number entered. Please try again"
+            }
+        }
+        
+        if numberOfTries > 0 {
+            if guessInt! == correctNumber {
+                guessStatusLabel.textColor = UIColor(hue: 0.35, saturation: 1, brightness: 0.87, alpha: 1.0)
+                guessStatusLabel.text = "You win! Click Reset game to play again!"
+            } else if guessInt! > correctNumber {
+                  guessStatusLabel.textColor = UIColor(hue: 0.675, saturation: 1, brightness: 0.81, alpha: 1.0)
+                numberOfTries -= 1
+                remainingAttemptsNumber.text = String(numberOfTries)
+                guessStatusLabel.text = "You're guess is too high. You have \(numberOfTries) left"
+            } else if guessInt! < correctNumber {
+                guessStatusLabel.textColor = UIColor(hue: 0.5, saturation: 1, brightness: 0.93, alpha: 1.0)
+                numberOfTries -= 1
+                remainingAttemptsNumber.text = String(numberOfTries)
+                guessStatusLabel.text = "You're guess is too low. You have \(numberOfTries) left"
+            }
+        } else {
+            guessStatusLabel.textColor = UIColor(hue: 0.9806, saturation: 1, brightness: 0.93, alpha: 1.0)
+            guessStatusLabel.text = "You lose! Click 'reset game' to try again"
+        }
+    }
+    
+    
+    
+    @IBAction func resetGameButtonPushed(_ sender: Any) {
+        numberOfTries = 5
+        remainingAttemptsNumber.text = String(numberOfTries)
+        guessStatusLabel.text = "Good luck!"
+        guessTextbox.text = "Enter a number between 1 and 100"
+    }
+    
     
 }
-
